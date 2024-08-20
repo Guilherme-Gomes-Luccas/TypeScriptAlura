cf8b920747436457ffc34140202115a828901c82
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2024-08-20 14:52:34 -0300

    Criação do metodo de retorno do array de negociacoes

--- app/controllers/negociao-controller.ts
+++ app/controllers/negociao-controller.ts
@@ -1,9 +1,11 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
-    private inputQuantidade: HTMLInputElement;;
-    private inputValor: HTMLInputElement;;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+    private negociacoes: Negociacoes = new Negociacoes();
 
     constructor() {
         this.inputData = document.querySelector('#data');
@@ -14,7 +16,9 @@ export class NegociacaoController {
     adiciona(): void {
         const negociacao = this.criaNegociao();
 
-        console.log(negociacao)
+        this.negociacoes.adiciona(negociacao);
+
+        console.log(this.negociacoes.lista());
         
         this.limparFormulario();
     }
