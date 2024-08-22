801f9822ecf229092981a60e698e275af470f8f0
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2024-08-22 09:28:08 -0300

    Liberando negociacoes somente em dias uteis

--- app/controllers/negociao-controller.ts
+++ app/controllers/negociao-controller.ts
@@ -18,17 +18,20 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
 
-    adiciona(): void {
+    public adiciona(): void {
         const negociacao = this.criaNegociao();
+        if (negociacao.data.getDay() > 0 && negociacao.data.getDay() < 6){
+            this.negociacoes.adiciona(negociacao);
+            this.limparFormulario();
+            this.atualizaView();
+        } else {
+            this.mensagemView.update('Apenas negociações em dias úteis são aceitas')
+        }
 
-        this.negociacoes.adiciona(negociacao);
-
-        this.negociacoesView.update(this.negociacoes);
-        this.mensagemView.update('Negociação adicionada com sucesso');
-        this.limparFormulario();
+        
     }
 
-    criaNegociao(): Negociacao {
+    private criaNegociao(): Negociacao {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value)
@@ -36,11 +39,16 @@ export class NegociacaoController {
         return new Negociacao(date, quantidade, valor);
     }
 
-    limparFormulario(): void {
+    private limparFormulario(): void {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
 
         this.inputData.focus();
+    }
+
+    private atualizaView(): void {
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso');
     }
 }
